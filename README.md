eacad166b5019d175df27cd087f3cb5e7ef7a80c
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-02-27 03:01:56 -0600

    Update and rename README.md to readme.rs

--- README.md
+++
@@ -1,2 +0,0 @@
-# Inspire.Channel
-Inspire { let channel = ['https://www.youtube.com/@machinethinking', 'https://www.youtube.com/@NewMind', 'https://www.youtube.com/@htme', 'https://www.youtube.com/@Blondihacks' } Favorite_List = ['[The Most Tortured Part In An Engine](https://youtu.be/Xy4tKWHTpY8)', '[The Science Of Flatness](https://youtu.be/lrfvbh6hCjM)']
---
+++ readme.rs
@@ -0,0 +1,6 @@
+// Inspire.Channel
+Inspire {
+  let channel = ['https://www.youtube.com/@machinethinking', 'https://www.youtube.com/@NewMind', 'https://www.youtube.com/@htme', 'https://www.youtube.com/@Blondihacks' };
+
+  let Favorite_List = ['[The Most Tortured Part In An Engine](https://youtu.be/Xy4tKWHTpY8)', '[Origins of Precision](https://youtu.be/gNRnrn5DE58)']
+}
